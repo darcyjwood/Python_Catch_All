29069360ecc7bfd261fbc3bc3e2fa5aa75c0d84b
EC2 Default User <ec2-user@ip-172-31-90-241.ec2.internal>
2022-06-22 16:44:25 +0000

    lab work

--- bio.py
+++ bio.py
@@ -1,9 +1,10 @@
 name = input("What is your name? ")
-color = input("What is your favorite color? ")
+color = input("What is your favorite food? ")
 age = int(input("How old are you today? "))
 
 # print(name, end=" ")
 # print("is " + str(age) + " years old", end=" ")
 # print("and loves the color " + color + ".", end=" ")
 
-print(name, 'is', age, 'years old and loves the color', color + '.')
+print(name, 'is', age, 'years old and loves the color', food + '.')
+
