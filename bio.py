7177cd65da0e4a07f2e35a513893d24cf7253c59
Darcy Wood <104536919+darcyjwood@users.noreply.github.com>
2022-06-21 22:31:50 -0500

    Create bio.py

---
+++ bio.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+age = int(input("How old are you today? "))
+
+# print(name, end=" ")
+# print("is " + str(age) + " years old", end=" ")
+# print("and loves the color " + color + ".", end=" ")
+
+print(name, 'is', age, 'years old and loves the color', color + '.')
