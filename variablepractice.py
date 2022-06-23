cdb75405a77d9b8f9b21422a458f96bf6846650a
EC2 Default User <ec2-user@ip-172-31-90-241.ec2.internal>
2022-06-23 03:01:32 +0000

    Price

--- variablepractice.py
+++ variablepractice.py
@@ -13,4 +13,14 @@ print(name + ", our specials for the day are " + specials + ".")
 
 order = input("What would you like to order?\n\n")
 
-print("Great! Your " + order + " will be ready shortly!")
+cost = 12
+
+quantity = input("How many would you like?\n")
+
+total = (cost * int(quantity))
+
+print("Thank you. Your total is: " + int(total) + "Thank you.")
+
+#print("Sounds great " + name + ", your price for that will be " + total)
+
+#print("Great! Your " + order + " will be ready shortly!")
