937b41e51cf5b1bf9c8febc9b2688a0415512c79
EC2 Default User <ec2-user@ip-172-31-90-241.ec2.internal>
2022-06-28 02:57:42 +0000

    #finalfizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+#fizzbuzz 
+upper_number = int(input('How many values should we process: '))
+
+for number in range(1, upper_number + 1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
