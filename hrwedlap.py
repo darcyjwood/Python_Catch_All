9c35f3456fd26602677123ba3a37b4eddb281f9a
EC2 Default User <ec2-user@ip-172-31-90-241.ec2.internal>
2022-07-02 23:19:57 +0000

    starting projects

--- hrwedlap.py
+++ hrwedlap.py
@@ -4,23 +4,49 @@
 #The first line contains a student's name.
 #The second line contains their grade.
 
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
+#if __name__ == '__main__':
+    #for _ in range(int(input())):
+#name = input()
+#score = float(input())
         
-     
-#students_grades.insert({['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]})
+#students_grades = ('name' + score)
+students_grades = {'Harry' : 37.21, 'Berry' : 37.21, 'Tina' : 37.2, 'Akriti': 41, 'Harsh' : 39}
+
+print(students_grades)
+students_grades.keys()
+print(sorted(list(students_grades.values())))
+x = (sorted(list(students_grades.values())))[1]
+print(x)
+y = (sorted(list(students_grades.values())))[2]
+print(y)
+
+students_grades.sort(key=lambda value: value[2])
+print(key)
 
 
-    list = [37.21, 37.21, 37.2, 41, 39]
-    max = list[0]
-    second_last = list[0]
-    for x in list[1:]:
-        if x > max:
-            second_last = max
-            max = x
-        elif x < max and x > second_last:
-            second_last = x
-    print(second_last)
+#print(students_grades.get(37.21))
+
+#for x in students_grades:
+#print(list(students_grades.keys [1, 2]))
+
+
+    #for x, k in students_grades.items():
+        #if k == 'x':
+        #print(k)
+
+
+#list(reversed(sorted(my_list)))
+#sort('score')
+#print(value)
+    
+    
+    
+    #second_last = list[0]
+    #for x in list[1:]:
+        #if x > max:
+            #second_last = max
+            #max = x
+        #elif x < max and x > second_last:
+            #second_last = x
+    #print(second_last)
         
