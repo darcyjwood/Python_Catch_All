cd10f8c7334835bc8d0ef545bfd7b7f58f34627d
EC2 Default User <ec2-user@ip-172-31-90-241.ec2.internal>
2022-07-05 00:25:59 +0000

    updates

--- dwood_070422.py
+++ dwood_070422.py
@@ -1,10 +1,24 @@
 import boto3
-aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket('totaltechnology')
+
+s3 = boto3.client('s3')
+
+response = s3.list_buckets()
+
+buckets = response["Buckets"]
+
+for bucket in buckets:
+    print(bucket["Name"])
+    
 response = bucket.create(
     ACL='public-read',
     CreateBucketConfiguration={
         'LocationConstraint': 'us-east-1'
-        },
+    },
+    GrantFullControl='string',
+    GrantRead='string',
+    GrantReadACP='string',
+    GrantWrite='string',
+    GrantWriteACP='string',
+    ObjectLockEnabledForBucket=True|False,
+    ObjectOwnership='BucketOwnerPreferred'|'ObjectWriter'|'BucketOwnerEnforced'
 )
-print(response)
