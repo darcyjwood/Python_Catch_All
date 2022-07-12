75ed22fa634901761960732e9b5019b7a8f2fa48
darcyjwood <darcyjwood@gmail.com>
2022-07-12 20:53:54 +0000

    more code and updates

--- ec2_create.py
+++ ec2_create.py
@@ -6,7 +6,7 @@ ec2_resource = boto3.resource("ec2")
 
 ec2_resource.create_instances(ImageId = 'ami-0cff7528ff583bf9a', 
         InstanceType = 't2.micro',
-        MaxCount = 1, 
-        MinCount = 1)
+        MaxCount = 3, 
+        MinCount = 3)
         
         
